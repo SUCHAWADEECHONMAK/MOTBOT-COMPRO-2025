40f5a1e6987fa75e24ef795fbfe864b99728da91
SUCHAWADEECHONMAK <68230082@kmitl.ac.th>
2025-07-18 19:33:09 +0700

    Update ex02.c

--- lab-1/ex02.c
+++ lab-1/ex02.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    printf("My name is YOUR_FIRST_NAME YOUR_LAST_NAMED\n");
-    printf("I am YOUR_AGE years old.\n");
-    printf("I am HEIGHT_IN_CM tall\n");
+    printf("Suchawadee Chomnak\n");
+    printf("19\n");
+    printf("163\n");
     return 0;
 }
